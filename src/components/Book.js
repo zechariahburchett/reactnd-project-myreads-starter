96ada580c9c30b8e70b63fa47aec564fe8f10aec
Zechariah Burchett <zechariah.burchett@gmail.com>
2018-09-29 22:26:45 -0400

    updated book component to use api data

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,7 +6,7 @@ class Book extends React.Component {
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
+            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${this.props.book.imageLinks.thumbnail || `""`})` }}></div>
             <div className="book-shelf-changer">
               <select>
                 <option value="move" disabled>Move to...</option>
@@ -17,8 +17,8 @@ class Book extends React.Component {
               </select>
             </div>
           </div>
-          <div className="book-title">The Hobbit</div>
-          <div className="book-authors">J.R.R. Tolkien</div>
+          <div className="book-title">{this.props.book.title}</div>
+          <div className="book-authors">{this.props.book.authors[0] || "No Author"}</div>
         </div>
       </li>
     );
